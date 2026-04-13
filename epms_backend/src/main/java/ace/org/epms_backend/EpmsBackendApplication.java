ad4fda9b213af1f6b6c8f5f1d22fb7d3472f5ab7
Myint Hlaing San <96964317+myinthlaingsan@users.noreply.github.com>
2026-04-13 21:24:12 +0630

    first commit

---
+++ epms_backend/src/main/java/ace/org/epms_backend/EpmsBackendApplication.java
@@ -0,0 +1,13 @@
+package ace.org.epms_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpmsBackendApplication.class, args);
+	}
+
+}
---
+++ epms_backend/src/test/java/ace/org/epms_backend/EpmsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ace.org.epms_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpmsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
