3e41018be352b1acdd7775b23583cd5872e6941d
minkhant202 <minkhant109023@gmail.com>
2026-04-24 14:12:33 +0630

    appraisal part update version

--- epms_backend/src/main/java/ace/org/epms_backend/service/DashboardService.java
+++
@@ -1,9 +0,0 @@
-package ace.org.epms_backend.service;
-
-import ace.org.epms_backend.dto.dashboard.*;
-
-public interface DashboardService {
-    EmployeeDashboardResponse getEmployeeDashboard(Long employeeId);
-    ManagerDashboardResponse getManagerDashboard(Long managerId);
-    AdminDashboardResponse getAdminDashboard();
-}
