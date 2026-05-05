20a97ada03fcd219b16a41e709a82e6cd7c330a9
AnnaGrace-J <maykyawtkhaing2842@gmail.com>
2026-05-05 23:08:48 +0630

    appraisal one backend

--- epms_backend/src/main/java/ace/org/epms_backend/service/AppraisalFormService.java
+++ epms_backend/src/main/java/ace/org/epms_backend/service/AppraisalFormService.java
@@ -5,8 +5,8 @@ import java.util.List;
 
 public interface AppraisalFormService {
     Long createForm(AppraisalFormRequest request);
-    void addCategory(Long formId, CategoryRequest request);
-    void addQuestion(Long categoryId, QuestionRequest request);
+    Long addCategory(Long formId, CategoryRequest request);
+    Long addQuestion(Long categoryId, QuestionRequest request);
     FullFormResponse getFullForm(Long formId);
     Long cloneForm(Long formId);
     void updateFormStatus(Long formId, Boolean isActive);
