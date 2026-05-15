1648b577f633f77497fe3a912a5d45728b108210
AnnaGrace-J <maykyawtkhaing2842@gmail.com>
2026-05-15 21:59:03 +0630

    appraisal edit&delete backend

--- epms_backend/src/main/java/ace/org/epms_backend/repository/QuestionRepository.java
+++ epms_backend/src/main/java/ace/org/epms_backend/repository/QuestionRepository.java
@@ -2,6 +2,9 @@ package ace.org.epms_backend.repository;
 
 import ace.org.epms_backend.model.appraisal.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -10,4 +13,14 @@ public interface QuestionRepository extends JpaRepository<Question, Long> {
     List<Question> findByCategory_CategoryId(Long categoryId);
 
     List<Question> findByCategory_Form_FormId(Long formId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Question q WHERE q.category.form.formId = :formId")
+    void deleteByFormId(Long formId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Question q WHERE q.category.categoryId = :categoryId")
+    void deleteByCategoryId(Long categoryId);
 }
