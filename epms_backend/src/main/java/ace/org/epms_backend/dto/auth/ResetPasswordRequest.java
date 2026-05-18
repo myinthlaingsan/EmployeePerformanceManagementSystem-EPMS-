419d6ce71650d69e4ab0e1f21c7d7e0e928dca00
Myint Hlaing San <96964317+myinthlaingsan@users.noreply.github.com>
2026-05-18 11:02:59 +0630

    modified password validation

--- epms_backend/src/main/java/ace/org/epms_backend/dto/auth/ResetPasswordRequest.java
+++ epms_backend/src/main/java/ace/org/epms_backend/dto/auth/ResetPasswordRequest.java
@@ -2,6 +2,7 @@ package ace.org.epms_backend.dto.auth;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +15,8 @@ public class ResetPasswordRequest {
     private String token;
     
     @NotBlank(message = "new password required")
-    @Size(min = 6, message = "password must be at least 6 characters")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", 
+             message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character (@$!%*?&)")
     private String newPassword;
 }
