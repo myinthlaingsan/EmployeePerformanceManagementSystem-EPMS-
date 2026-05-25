6e98b5deda19510fdebb73457858422ae9370bc2
Myint Hlaing San <96964317+myinthlaingsan@users.noreply.github.com>
2026-05-25 13:18:45 +0630

    modified password and other permission

--- epms_backend/src/main/java/ace/org/epms_backend/repository/RoleRepository.java
+++ epms_backend/src/main/java/ace/org/epms_backend/repository/RoleRepository.java
@@ -5,11 +5,12 @@ import ace.org.epms_backend.model.employee.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByRoleName(RoleType roleName);
-
+    List<Role> findAllByOrderByRoleIdAsc();
     boolean existsByRoleName(RoleType roleName);
 }
