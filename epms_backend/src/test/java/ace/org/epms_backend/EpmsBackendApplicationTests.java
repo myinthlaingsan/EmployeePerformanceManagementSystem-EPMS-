b139ec0d3e73907038b1731864fd2b6612c04403
Myint Hlaing San <96964317+myinthlaingsan@users.noreply.github.com>
2026-05-27 10:30:08 +0630

    feedbackreportservice and summary and calibration modified

--- epms_backend/src/test/java/ace/org/epms_backend/EpmsBackendApplicationTests.java
+++ epms_backend/src/test/java/ace/org/epms_backend/EpmsBackendApplicationTests.java
@@ -2,13 +2,6 @@ package ace.org.epms_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import net.sf.jasperreports.engine.JasperCompileManager;
-
-import java.io.InputStream;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
 
 @SpringBootTest
 class EpmsBackendApplicationTests {
@@ -16,22 +9,4 @@ class EpmsBackendApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
-	@Test
-	void testAllReportTemplatesCompile() {
-		try {
-			PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-			Resource[] resources = resolver.getResources("classpath:reports/**/*.jrxml");
-			for (Resource resource : resources) {
-				System.out.println("Compiling report template: " + resource.getFilename());
-				try (InputStream is = resource.getInputStream()) {
-					assertNotNull(JasperCompileManager.compileReport(is), "Failed to compile: " + resource.getFilename());
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			fail("Compilation of report templates failed: " + e.getMessage());
-		}
-	}
-
 }
